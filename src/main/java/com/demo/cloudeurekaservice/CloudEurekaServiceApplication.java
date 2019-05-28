5366be8aece88b3feb01fd24d7c25a830f7045bd
weixy <827928660@qq.com>
2019-05-28 14:37:22 +0800

    'init'

---
+++ src/main/java/com/demo/cloudeurekaservice/CloudEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.demo.cloudeurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //启动服务注册中心
+@SpringBootApplication
+public class CloudEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaServiceApplication.class, args);
+	}
+
+}
